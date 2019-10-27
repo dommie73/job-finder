7b7aed590e04662d7844adfeaf92455a81e87630
Dominika Słowik <dommie73@gmail.com>
2019-10-27 23:54:54 +0100

    Fix issues with the server
    - enable CORS
    - fix `service` import in `controller.js`
    - remove unused file extensions from import statements

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const HOSTNAME = '127.0.0.1';
 const PORT = 4723;
 
-const server = require('./controller.js');
+const server = require('./controller');
 
 server.listen(PORT, HOSTNAME, () => {
   console.log(`Server is running at ${HOSTNAME}:${PORT}`);
--- src/service.js
+++ src/service.js
@@ -1,25 +1,36 @@
 const api = require('./scrapers/api');
 
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'OPTIONS, GET'
+};
+
 const jobsRequest = async (req, res, query) => {
   try {
     const data = await api.getJobOffers(query);
 
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.writeHead(200, { ...headers, 'Content-Type': 'application/json' });
     res.end(JSON.stringify(data));
   }
 
   catch (error) {
-    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.writeHead(500, { ...headers, 'Content-Type': 'text/plain' });
     res.end('Oops, something went wrong :c');
   }
 };
 
+const cors = (req, res) => {
+  res.writeHead(204, headers);
+  res.end();
+};
+
 const invalidRequest = (req, res) => {
-  res.writeHead(404, { 'Content-Type': 'text/plain' });
+  res.writeHead(404, { ...headers, 'Content-Type': 'text/plain' });
   res.end('Invalid request.');
 };
 
 module.exports = {
   jobsRequest,
-  invalidRequest
+  invalidRequest,
+  cors
 };
