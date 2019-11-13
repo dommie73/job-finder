914dcf905ecf35f50c9f4693f23aff7c580b221c
Dominika Słowik <dommie73@gmail.com>
2019-11-13 23:07:06 +0100

    Setup MongoDB Atlas

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const app = require("./src/app");
 const { PORT } = require("./src/config");
 
+require("./src/db");
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
