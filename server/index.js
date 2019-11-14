761047adc0778512efd93675e7770d5dfc096796
Dominika Słowik <dommie73@gmail.com>
2019-11-14 01:10:32 +0100

    Modify `/search` route to scrape and return data

--- server/index.js
+++ server/index.js
@@ -1,11 +1,8 @@
 const { PORT } = require("./src/config");
 
 const app = require("./src/app");
-const router = require("./src/express/routes");
 
 require("./src/db");
-
-app.use("/", router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const cors = require("cors");
+
+const router = require("./express/routes");
 
 const app = express();
+
+app.use(cors());
+app.use("/", router);
 
 module.exports = app;
